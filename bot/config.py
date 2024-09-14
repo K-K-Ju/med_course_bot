c4ed9fc22e6bf18743d0cb2e46e1cbf3b69ac6be
K-K-Ju <andrunivermail@gmail.com>
2024-09-14 17:13:12 +0300

    Added reading of logging level and logging file path from config file.

--- bot/config.py
+++ bot/config.py
@@ -1,14 +1,18 @@
 import json
+import logging
 
 config = {}
 
 
 def __load_config__(path):
     with open(path) as secrets_file:
-        secrets = json.load(secrets_file)
-        return secrets
+        cfg = json.load(secrets_file)
+        return cfg
 
 
 def init_config(path):
-    secrets = __load_config__(path)
-    config['ADMIN_KEY'] = secrets.get('ADMIN_KEY')
+    cfg = __load_config__(path)
+    config['ADMIN_KEY'] = cfg.get('ADMIN_KEY')
+    config['LOG_LVL'] = logging.getLevelNamesMapping()[cfg.get('LOG_LVL')]
+    config['LOG_FILE_PATH'] = cfg.get('LOG_FILE_PATH')
+
